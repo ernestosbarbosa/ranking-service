4008f8c7b57c38229809fae0b9cf3f6f4add6461
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2020-05-29 14:42:08 -0300

    variacao de responses

--- controllers/ranking.ts
+++ controllers/ranking.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import * as queue from 'express-queue'
 import * as timeout from 'express-timeout-handler'
 import * as cors from 'cors'
@@ -6,13 +6,17 @@ import * as bodyParser from 'body-parser'
 import * as loki from 'lokijs'
 import { v4 as uuidv4 } from 'uuid'
 import { check, validationResult } from 'express-validator'
+import * as rateLimit from 'express-rate-limit'
+import * as delay from 'randelay'
 
 const router: Router = Router();
 const db = new loki('db.json');
 const rankingTable = db.addCollection('ranking');
 
+const MAX_CONNECTIONS = 50;
+const TIMEOUT = 15000;
 const options = {
-    timeout: 300000,
+    timeout: TIMEOUT,
 
     onTimeout: function (req: Request, res: Response) {
         res.status(503).send('Service unavailable. Please retry.');
@@ -20,11 +24,25 @@ const options = {
 };
 
 router.use(cors());
-// router.use(bodyParser.json());
 router.use(bodyParser.json({ limit: '50mb' }));
 router.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 router.use(queue({ activeLimit: 1, queuedLimit: -1 }));
 router.use(timeout.handler(options));
+router.use(rateLimit(
+    {
+        max: MAX_CONNECTIONS,
+        windowMs: 1000,
+        message: "Max connections",
+        onLimitReached: function (req, res, options) {
+            //console.log(req.rateLimit)
+        }
+    }
+))
+router.use((req: Request, res: Response, next: NextFunction) => {
+    delay(100, '17s').then(() => {
+        next();
+    })
+});
 
 router.get('/', (req: Request, res: Response) => {
     return res.status(501).send();
@@ -53,6 +71,15 @@ router.post('/',[
     check('tentativas').isNumeric(),
     check('timestamp').isNumeric()
 ], (req: Request, res: Response) => {
+    if((Math.random() * 10) < 3){
+        return res.status(200).send({
+            erro: {
+                mensagem: "Database sync failed",
+                code: "AAFF0099"
+            }
+        });
+    }
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
